a396c75f0373cc392c8132319c024103258d2a48
Tranhongthu <thu.th19010031@st.phenikaa-uni.edu.vn>
2023-04-03 18:44:59 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import React from 'react';
 import './App.scss';
 import Profile from './pages/Profile';
 import Logout from './pages/Authentication/components/Logout';
+import Login from './pages/Authentication/components/Login';
 
 function App() {
   return (
     <Layout style={{ padding: '0', backgroundColor: 'white' }}>
       <Content style={{ padding: '0 50px' }} className="contentLayout">
         <Logout />
+        <Login />
         <Profile />
       </Content>
     </Layout>
